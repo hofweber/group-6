72ca0e887ed8d2998242e48f00bd9b66d82f6a23
Patrick Hofweber <patchh@gmail.com>
2021-02-26 10:38:43 +0100

    Swapped in original code
    Can’t get this to work. “The AudioContext is "suspended". Invoke Tone.start() from a user action to start the audio.”

--- patch/looper/app.js
+++ patch/looper/app.js
@@ -1,28 +1,27 @@
-function sequencer(){
-    const kick = new Tone.Player('./samples/kick-808.wav').toMaster();
-    const clap = new Tone.Player('./samples/clap-808.wav').toMaster();
+function sequencer() {
+    const kick = new Tone.Player("./samples/kick-808.wav").toMaster();
+    const clap = new Tone.Player("./samples/clap-808.wav").toMaster();
     let index = 0;
-
+  
     Tone.Transport.scheduleRepeat(repeat, "8n");
     Tone.Transport.start();
-
+  
     function repeat() {
-        let step = index % 8;
-        console.log(step);
-        let kickInputs = document.querySelector(
-            `.kick input:nth-child(${step + 1})`
-        );
-        let clapInputs = document.querySelector(
-            `.clap input:nth-child(${step + 1})`
-        );
-        if(kickInputs.checked) {
-            kick.start();
-        }
-        if(clapInputs.checked) {
-            clap.start();
-        }
-        index++;
+      let step = index % 8;
+      let kickInputs = document.querySelector(
+        `.kick input:nth-child(${step + 1})`
+      );
+      let snareInputs = document.querySelector(
+        `.snare input:nth-child(${step + 1})`
+      );
+      if (kickInputs.checked) {
+        kick.start();
+      }
+      if (snareInputs.checked) {
+        snare.start();
+      }
+      index++;
     }
-}
-
-sequencer();
+  }
+  
+  sequencer();
