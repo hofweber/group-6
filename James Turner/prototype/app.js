d7c467199f348cf5ad5f944cb7239c9a4e5da313
JamesTurner88 <78788785+JamesTurner88@users.noreply.github.com>
2021-03-08 13:44:04 +0100

    Create app.js
    First attempt my protoype

---
+++ James Turner/prototype/app.js
@@ -0,0 +1,39 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+//var five = require('johnny-five');
+//const board = new Board();
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  http.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
+
+ // board.on("ready", () => {
+    // Create a standard `led` component
+    // on a valid pwm pin
+   // const led = new Led(11);
+
+if (heldDown = true) {
+    //led.pulse(),
+    console.log("button held down");
+} else {
+    //led.stop().off(),
+    console.log("button not held down");
+};
+
+   // led.pulse();
+
+    // Stop and turn off the led pulse loop after
+    // 10 seconds (shown in ms)
+   // board.wait(10000, () => {
+  
+      // stop() terminates the interval
+      // off() shuts the led off
+     // led.stop().off();
+   // });
+ // });
