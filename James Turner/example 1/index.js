92c143154a0faec28b68e1b30a13404b67981294
JamesTurner88 <78788785+JamesTurner88@users.noreply.github.com>
2021-03-08 13:51:33 +0100

    Create index.js
    First example with socket.io, a simple chat

---
+++ James Turner/example 1/index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    //sends chat mesages to all users
+   io.emit('chat message', msg);
+  });
+});
+//check that the server is running on port 3000
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
