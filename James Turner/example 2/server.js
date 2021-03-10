834cc8767a6fab47a8eb3cb296a876072ff8207a
JamesTurner88 <78788785+JamesTurner88@users.noreply.github.com>
2021-03-10 21:30:08 +0100

    Update server.js
    attempting to change the example but the button counters do not work

--- James Turner/example 2/server.js
+++ James Turner/example 2/server.js
@@ -1,29 +1,36 @@
 // server.js
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app); 
-var io = require('socket.io')(server); 
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
 //keep track of how times clients have clicked the button
-var clickCount = 0;
+var clickCount1 = 0;
+var clickCount2 = 0;
 
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + "/public"));
 //redirect / to our index.html file
-app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", function (req, res, next) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-io.on('connection', function(client) { 
-	console.log('Client connected...'); 
-	//when the server receives clicked message, do this
-    client.on('clicked', function(data) {
-    	  clickCount++;
-		  //send a message to ALL connected clients
-		  io.emit('buttonUpdate', clickCount);
-    });
+io.on("connection", function (client) {
+  console.log("Client connected...");
+  //when the server receives clicked message, do this
+  client.on("clicked1", function (data) {
+    clickCount1++;
+    //send a message to ALL connected clients
+    io.emit("button1Update", clickCount1);
+  });
+  client.on("clicked2", function (data) {
+    clickCount2++;
+    //send a message to ALL connected clients
+    io.emit("button2Update", clickCount2);
+  });
 });
 
 //start our web server and socket.io server listening
-server.listen(3000, function(){
-  console.log('listening on *:3000');
-}); 
+server.listen(3000, function () {
+  console.log("listening on *:3000");
+});
+
