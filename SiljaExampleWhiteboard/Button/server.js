cfb7cdff58a6e8699bd0b35813a4c84420e1eaa8
SiljaSofie <78904431+SiljaSofie@users.noreply.github.com>
2021-03-10 15:19:01 +0100

    Update server.js
    Adding a button with sound to browser

--- SiljaExampleWhiteboard/Button/server.js
+++ SiljaExampleWhiteboard/Button/server.js
@@ -1,30 +1,42 @@
-// server.js
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app); 
-var io = require('socket.io')(server); 
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Socket.io Demo</title>
+      <meta charset="utf-8">
+		<link rel="stylesheet" href="styles.css">
+   </head>
+   <body>
+		 <h1>Socket.io Demo</h1>	 
+		 <p id="buttonCount">The button has been clicked 0 times.</p>
+		 <button onclick="buttonClicked()">Click me</button>
+		 <button onclick="playAudio()" type="button">Play Audio</button>
+		 <button onclick="pauseAudio()" type="button">Pause Audio</button> 
+		 <audio id="audio" src="Bees.mp3"></audio>
+		 <script src="/socket.io/socket.io.js"></script>
+		 <script>
+			 var socket = io.connect();
+			 
+			 function buttonClicked(){
+			   socket.emit('clicked');
+			 }
+			 
+			 //when we receive buttonUpdate, do this
+			 socket.on('buttonUpdate', function(data){
+				 document.getElementById("buttonCount").innerHTML = 'The button has been clicked ' + data + ' times.';
+			 });
+             
+			 //Add audio to the button
+			 var x = document.getElementById("audio"); 
 
-//keep track of how times clients have clicked the button
-var clickCount = 0;
+			 function playAudio() { 
+				x.play(); 
+				} 
 
-app.use(express.static(__dirname + '/public')); 
-//redirect / to our index.html file
-app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-io.on('connection', function(client) { 
-	console.log('Client connected...'); 
-	//when the server receives clicked message, do this
-    client.on('clicked', function(data) {
-        clickCount++;
-        //send a message to ALL connected clients
-        io.emit('buttonUpdate', clickCount);
-  });
-});
-
-//start our web server and socket.io server listening
-server.listen(3000, function(){
-  console.log('listening on *:3000');
-}); 
+			 function pauseAudio() { 
+				x.pause(); 
+				} 
+			</script>
+		</script>
+   </body>
+</html>
 
