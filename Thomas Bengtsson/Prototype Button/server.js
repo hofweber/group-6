3b25e9ed96bed7dfb419ee8b11cf1e314bdae872
Thomas-Bengtsson <78793117+Thomas-Bengtsson@users.noreply.github.com>
2021-03-10 23:38:00 +0100

    Update server.js

--- Thomas Bengtsson/Prototype Button/server.js
+++ Thomas Bengtsson/Prototype Button/server.js
@@ -7,28 +7,20 @@ console.log("Listening to requests on port 3000")
 
 var io = socket(server);
 
-//keeps count the number of clicks the button has left. Starts att 50 clicks
-var clickCount = 1000;
+app.use(express.static('public'));
 
-//allows the use for CSS
-app.use(express.static(__dirname + '/public')); 
-//redirect / to our index.html file
-app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/public/index.html');
-});
+io.on('connection', function(socket) {
+console.log('New transmitter', socket.id);
 
-//when client connects to the browser it will say "CLient connected..." in terminal
-io.on('connection', function(client) { 
-	console.log('Client connected...'); 
-	//when the button is clicked the click counter will be reduced by 1
-    client.on('clicked', function(data) {
-    	  clickCount--;
-		  //this updates the click counter for all the clients
-		  io.emit('buttonUpdate', clickCount);
+
+    socket.on('first', function(data) {
+        socket.emit('first', data);
+        console.log('Long press from' + socket.id);
+
+    socket.on('second', function(data) {
+        socket.emit('second', data);
+        console.log('second button has been pressed from' + socket.id);
+    })
+     
     });
-});
-
-//makes web server and socket.io server start listening to port 3000
-server.listen(3000, function(){
-  console.log('listening on *:3000');
 });
