2dce9097ca8b8fdc5396009407228c03154cbcda
JamesTurner88 <78788785+JamesTurner88@users.noreply.github.com>
2021-03-11 12:01:13 +0100

    Update app.js
    Board timeout fixed

--- James Turner/prototype/app.js
+++ James Turner/prototype/app.js
@@ -2,8 +2,9 @@ const app = require("express")();
 const http = require("http").Server(app);
 const socket = require("socket.io")(http);
 var five = require("johnny-five");
+const {Board, Led} = require("johnny-five");
 
-const board = new five.Board();
+const board = new Board({ port: "COM3" });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -15,19 +16,19 @@ http.listen(3000, () => {
 
 board.on("ready", () => {
   //Create a standard `led` component
- //on a valid pwm pin
- const led = new Led(11);
+  //on a valid pwm pin
+  const led = new Led(11);
 
-socket.on("buttonDown", () => {
-  console.log("someone pressed the button");
-  led.pulse(); 
-  socket.emit("startLed");
+  socket.on("buttonDown", () => {
+    console.log("someone pressed the button");
+    led.pulse();
+    socket.emit("startLed");
+  });
+
+  socket.on("buttonUp", () => {
+    console.log("someone released the button");
+    led.stop().off();
+    socket.emit("stopLed");
+  });
 });
 
-socket.on("buttonUp", () => {
-  console.log("someone released the button");
-  led.stop().off();
-  socket.emit("stopLed");
-});
-
-});
