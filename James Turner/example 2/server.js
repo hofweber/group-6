e900d7ac8fe5742e878b8a6177c071096adb33ac
JamesTurner88 <78788785+JamesTurner88@users.noreply.github.com>
2021-03-11 22:11:10 +0100

    Update server.js
    Changed the concept to one button adding and the other subtracting and now it works.

--- James Turner/example 2/server.js
+++ James Turner/example 2/server.js
@@ -5,8 +5,8 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
 //keep track of how times clients have clicked the button
-var clickCount1 = 0;
-var clickCount2 = 0;
+var clickCount = 0;
+
 
 app.use(express.static(__dirname + "/public"));
 //redirect / to our index.html file
@@ -18,16 +18,16 @@ io.on("connection", function (client) {
   console.log("Client connected...");
   //when the server receives clicked message, do this
   client.on("clicked1", function (data) {
-    clickCount1++;
-    console.log("button 1 " + clickCount1);
+    clickCount++;
+    console.log("button 1 " + clickCount);
     //send a message to ALL connected clients
-    io.emit("button1Update", clickCount1);
+    io.emit("buttonUpdate", clickCount);
   });
   client.on("clicked2", function (data) {
-    clickCount2++;
-    console.log("button 2 " + clickCount2);
+    clickCount--;
+    console.log("button 2 " + clickCount);
     //send a message to ALL connected clients
-    io.emit("button2Update", clickCount2);
+    io.emit("buttonUpdate", clickCount);
   });
 });
 
@@ -35,3 +35,4 @@ io.on("connection", function (client) {
 server.listen(3000, function () {
   console.log("listening on *:3000");
 });
+
