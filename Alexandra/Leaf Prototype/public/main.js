c36bb6ea1827392b0cbb543cffcd020d0644b9ba
alexandraraducanu <79511238+alexandraraducanu@users.noreply.github.com>
2021-03-11 23:09:40 +0100

    made the leaf change colors when clicked

--- Alexandra/Leaf Prototype/public/main.js
+++ Alexandra/Leaf Prototype/public/main.js
@@ -20,7 +20,7 @@
     
   }
 
-  function drawLeaf(color) {
+  function drawLeaf(color, emit) {
     var ctx = canvas.getContext('2d');
     var x,y
     ctx.beginPath();     // Start a new path.
@@ -45,6 +45,14 @@
     
     ctx.stroke();
     ctx.fill();
+
+
+    if (!emit) { return; }
+
+
+    socket.emit('drawing', {
+      color: 'red'
+    });
     
     console.log('it works');
 
@@ -67,19 +75,15 @@
 
 
   function onMouseDown(e) {
-    drawLeaf('red');
+    drawLeaf('red', true);
     drawing = true;
     current.x = e.clientX||e.touches[0].clientX;
     current.y = e.clientY||e.touches[0].clientY;
 
-    socket.emit('drawing', {
-      color: 'red'
-    });
   }
-
-
-
+ 
   function onMouseUp(e){
+    drawLeaf("green", true);
     if (!drawing) { return; }
     drawing = false; 
     // drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
@@ -101,6 +105,7 @@
 
 
   function onDrawingEvent(data){
+    drawLeaf(data.color);
     var w = canvas.width;
     var h = canvas.height;
     //drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
